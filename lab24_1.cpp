0fb3ab36d32ce9f0cd73c2a01b3d67007c2b6a91
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2024-03-02 12:52:33 +0700

    add 24.1

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -65,6 +65,27 @@ void List::append(int d){
 
 //Write List::remove() here
 
-void List::remove(int num){
-	
+void List::remove(int d)
+{
+	Node *F = root;
+	Node *S = root;
+	if (d == 0)
+	{
+		root = root->next;
+		delete F;
+	}
+	else
+	{
+		for (int i = 0; i < d; i++)
+		{
+			F = F->next;
+		}
+		for (int i = 0; i < d - 1; i++)
+		{
+			S = S->next;
+		}
+		S->next = F->next;
+		delete F;
+	}
+	size--;
 }
